66e06cdc3c404c0024fbc189bd0ca5d954407ef4
maheshtns92 <>
2026-03-17 00:38:56 +0000

    For loops

---
+++ the_for_loop/for_loops/main.py
@@ -0,0 +1,7 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+travel_list = []
+for country in countries:
+    travel_list.append(country)
+# Testing
+print("Countries to visit:", travel_list)
